56a4beb5ea3caecae55427ecff60b519108f6983
Lior <liormdevops@gmail.com>
2024-01-15 12:11:10 +0200

    logs format changed to standard

--- app/main.py
+++ app/main.py
@@ -26,23 +26,41 @@ class CreatePostForm(FlaskForm):
     body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
-class JsonFormatter(logging.Formatter):
-    def format(self, record):
-        log_record = {
-            "time": self.formatTime(record, self.datefmt),
-            "level": record.levelname,
-            "message": record.getMessage()
-        }
-        if record.exc_info:
-            log_record["exc_info"] = self.formatException(record.exc_info)
-        return json.dumps(log_record)
+# Define a standard formatter
+class StandardFormatter(logging.Formatter):
+    def __init__(self, fmt=None, datefmt=None):
+        super().__init__(fmt, datefmt)
 
 # Setup logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+# Use a more human-readable format for the logs
+standard_format = '%(asctime)s - %(levelname)s - %(message)s'
+date_format = '%Y-%m-%d %H:%M:%S'
+formatter = StandardFormatter(standard_format, date_format)
+
 log_handler = logging.StreamHandler()
-log_handler.setFormatter(JsonFormatter())
+log_handler.setFormatter(formatter)
 logger.addHandler(log_handler)
+
+# class JsonFormatter(logging.Formatter):
+#     def format(self, record):
+#         log_record = {
+#             "time": self.formatTime(record, self.datefmt),
+#             "level": record.levelname,
+#             "message": record.getMessage()
+#         }
+#         if record.exc_info:
+#             log_record["exc_info"] = self.formatException(record.exc_info)
+#         return json.dumps(log_record)
+
+# # Setup logging
+# logger = logging.getLogger()
+# logger.setLevel(logging.INFO)
+# log_handler = logging.StreamHandler()
+# log_handler.setFormatter(JsonFormatter())
+# logger.addHandler(log_handler)
 
 @app.route('/')
 def get_all_posts():
