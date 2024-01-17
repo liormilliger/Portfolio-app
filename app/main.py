8ef2e7647c5dc41defb1600f247b647c689c3767
Lior <liormdevops@gmail.com>
2024-01-17 20:19:48 +0200

    testing ok route alone

--- app/main.py
+++ app/main.py
@@ -1,107 +1,110 @@
-from flask import Flask, render_template, redirect, url_for, abort, request
-from flask_bootstrap import Bootstrap
-from flask_pymongo import PyMongo
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, URL
-from flask_ckeditor import CKEditor, CKEditorField
-from datetime import date
-from bson import ObjectId
-import os
+from flask import Flask
+# , render_template, redirect, url_for, abort, request
+# from flask_bootstrap import Bootstrap
+# from flask_pymongo import PyMongo
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, SubmitField
+# from wtforms.validators import DataRequired, URL
+# from flask_ckeditor import CKEditor, CKEditorField
+# from datetime import date
+# from bson import ObjectId
+# import os
 from prometheus_flask_exporter import PrometheusMetrics
 
-# import metrics
+# # import metrics
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
-metrics.info('app_info', 'Application info', version='1.0.3')
+# metrics.info('app_info', 'Application info', version='1.0.3')
 
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-app.config['MONGO_URI'] = os.environ.get('MONGO_URI')  # Set your MongoDB URI
-mongo = PyMongo(app)
-Bootstrap(app)
-ckeditor = CKEditor(app)
+# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# app.config['MONGO_URI'] = os.environ.get('MONGO_URI')  # Set your MongoDB URI
+# mongo = PyMongo(app)
+# Bootstrap(app)
+# ckeditor = CKEditor(app)
 
-
-# import metrics
-
-class CreatePostForm(FlaskForm):
-    title = StringField("Blog Post Title", validators=[DataRequired()])
-    subtitle = StringField("Subtitle", validators=[DataRequired()])
-    author = StringField("Your Name", validators=[DataRequired()])
-    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = CKEditorField("Blog Content", validators=[DataRequired()])
-    submit = SubmitField("Submit Post")
-    
 @app.route('/')
-# @metrics.counter('invocation_by_type', 'Number of invocations by type',
-#          labels={'item_type': lambda: request.view_args['type']})
-def get_all_posts():
-    posts = mongo.db.blog.find()
-    return render_template("index.html", all_posts=posts)
+def main():
+    return 'OK'
+# # import metrics
+
+# class CreatePostForm(FlaskForm):
+#     title = StringField("Blog Post Title", validators=[DataRequired()])
+#     subtitle = StringField("Subtitle", validators=[DataRequired()])
+#     author = StringField("Your Name", validators=[DataRequired()])
+#     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+#     body = CKEditorField("Blog Content", validators=[DataRequired()])
+#     submit = SubmitField("Submit Post")
     
-def get_post(post_id):
-    try:
-        oid = ObjectId(post_id)
-    except:
-        abort(404)
-    return oid
+# @app.route('/')
+# # @metrics.counter('invocation_by_type', 'Number of invocations by type',
+# #          labels={'item_type': lambda: request.view_args['type']})
+# def get_all_posts():
+#     posts = mongo.db.blog.find()
+#     return render_template("index.html", all_posts=posts)
+    
+# def get_post(post_id):
+#     try:
+#         oid = ObjectId(post_id)
+#     except:
+#         abort(404)
+#     return oid
 
-@app.route("/post/<post_id>")
-def show_post(post_id):
-    requested_post = mongo.db.blog.find_one_or_404({"_id": get_post(post_id)})
-    return render_template("post.html", post=requested_post)
+# @app.route("/post/<post_id>")
+# def show_post(post_id):
+#     requested_post = mongo.db.blog.find_one_or_404({"_id": get_post(post_id)})
+#     return render_template("post.html", post=requested_post)
 
-@app.route("/new-post", methods=["GET", "POST"])
-def add_new_post():
-    form = CreatePostForm()
-    if form.validate_on_submit():
-        new_post = {
-            "title": form.title.data,
-            "subtitle": form.subtitle.data,
-            "body": form.body.data,
-            "img_url": form.img_url.data,
-            "author": form.author.data,
-            "date": date.today().strftime("%B, %d, %Y")
-        }
-        mongo.db.blog.insert_one(new_post)
-        return redirect(url_for("get_all_posts"))
-    return render_template("make-post.html", form=form)
+# @app.route("/new-post", methods=["GET", "POST"])
+# def add_new_post():
+#     form = CreatePostForm()
+#     if form.validate_on_submit():
+#         new_post = {
+#             "title": form.title.data,
+#             "subtitle": form.subtitle.data,
+#             "body": form.body.data,
+#             "img_url": form.img_url.data,
+#             "author": form.author.data,
+#             "date": date.today().strftime("%B, %d, %Y")
+#         }
+#         mongo.db.blog.insert_one(new_post)
+#         return redirect(url_for("get_all_posts"))
+#     return render_template("make-post.html", form=form)
 
-@app.route("/edit_post/<post_id>", methods=["GET", "PUT", "POST"])
-def edit_post(post_id):
-    post = mongo.db.blog.find_one_or_404({"_id": get_post(post_id)})
-    edit_form = CreatePostForm(
-        title=post["title"],
-        subtitle=post["subtitle"],
-        img_url=post["img_url"],
-        author=post["author"],
-        body=post["body"],
-    )
-    if edit_form.validate_on_submit():
-        updated_post = {
-            "title": edit_form.title.data,
-            "subtitle": edit_form.subtitle.data,
-            "img_url": edit_form.img_url.data,
-            "author": edit_form.author.data,
-            "body": edit_form.body.data,
-        }
-        mongo.db.blog.update_one({"_id": get_post(post_id)}, {"$set": updated_post})
-        return redirect(url_for("show_post", post_id=get_post(post_id)))
-    return render_template("make-post.html", form=edit_form, is_edit=True)
+# @app.route("/edit_post/<post_id>", methods=["GET", "PUT", "POST"])
+# def edit_post(post_id):
+#     post = mongo.db.blog.find_one_or_404({"_id": get_post(post_id)})
+#     edit_form = CreatePostForm(
+#         title=post["title"],
+#         subtitle=post["subtitle"],
+#         img_url=post["img_url"],
+#         author=post["author"],
+#         body=post["body"],
+#     )
+#     if edit_form.validate_on_submit():
+#         updated_post = {
+#             "title": edit_form.title.data,
+#             "subtitle": edit_form.subtitle.data,
+#             "img_url": edit_form.img_url.data,
+#             "author": edit_form.author.data,
+#             "body": edit_form.body.data,
+#         }
+#         mongo.db.blog.update_one({"_id": get_post(post_id)}, {"$set": updated_post})
+#         return redirect(url_for("show_post", post_id=get_post(post_id)))
+#     return render_template("make-post.html", form=edit_form, is_edit=True)
 
-@app.route("/delete/<post_id>", methods=["GET", "DELETE"])
-def delete_post(post_id):
-    mongo.db.blog.delete_one({"_id": get_post(post_id)})
-    return redirect(url_for('get_all_posts'))
+# @app.route("/delete/<post_id>", methods=["GET", "DELETE"])
+# def delete_post(post_id):
+#     mongo.db.blog.delete_one({"_id": get_post(post_id)})
+#     return redirect(url_for('get_all_posts'))
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+# @app.route("/about")
+# def about():
+#     return render_template("about.html")
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+# @app.route("/contact")
+# def contact():
+#     return render_template("contact.html")
 
 # -----------------[ Metrics Start ]---------------------
 
@@ -132,12 +135,12 @@ def contact():
 #     return "Blog Content"
 
 # Initialize the metrics endpoint
-metrics.register_default(
-    metrics.counter(
-        'by_path_counter', 'Request count by request paths',
-        labels={'path': lambda: request.path}
-    )
-)
+# metrics.register_default(
+#     metrics.counter(
+#         'by_path_counter', 'Request count by request paths',
+#         labels={'path': lambda: request.path}
+#     )
+# )
 # -------------------[ Metrics End ] -------------------------
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
